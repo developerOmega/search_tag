a1a4543e825380467f0252a87cd1848bf3c186dd
developerOmega <developerOmega@developerOmega.com>
2019-10-31 14:48:38 -0600

    Documentar vercion 2 de objeto

--- search_tags_v2.js
+++ search_tags_v2.js
@@ -1,20 +1,36 @@
 class SearchTag{
-    publication_option = document.getElementById("contentTags");
-    newData = [];
+    publication_option = document.getElementById("contentTags"); 
+    //Propiedad con etiqueta id 'contentTags: etiqueta donde se publican los tags
+
     field;
+    //Propiedad que recibirá el input del parámetro del método constructor 'inputTags';
+
     data;
+    //Propiedad que recibirá los datos tipo objeto del parametro del metodo constructor 'dataValue'
+
     constructor(dataValue, inputTags){
         this.field = document.getElementById(inputTags);
         this.data = dataValue;
     }
 
+    //Método searchTags; crea las tags en base a los usuarios; como parámetro recibe un array con elementos div de los datos existentes
     searchTags(options){
+
+        //Se hace una validación por medio de expresiones regulares, validando la existencia del carácter '@' 
         if(this.field.value.match(/#[a-zA-Z0-9]+/)){
+
+            //La propiedad 'field' se convierte en un array de caracteres; 'field' = input con la información de la búsqueda
+            //Se elimina el primer elemento del array, el cual es el "@"
+            //El array se convierte en una cadena de texto nuevamente
             let value = this.field.value.split("");
             value.shift();
             value = value.join("");
+            /*---------------------------------------------------------- */
             
+            //Ciclo que recorre todos los datos de la propiedad 'data'
             for(let i = 0; i < this.data.length; i++){
+
+                //Condición para crear los datos existentes de la información recibida por la variable 'value'; En caso de ser -1 los elementos no tendran informacion y no se mostrarán en la página
                 if(this.data[i].name.indexOf(value) > -1){
                     options[i].value = this.data[i].id;
                     options[i].innerHTML = "<img src= '" +this.data[i].photo + "' > " +  "<div> <p>" + this.data[i].name + "</p> <p>" +  this.data[i].type + "</p></div>";
@@ -29,14 +45,21 @@ class SearchTag{
         }
     }
 
+    //Método para crear los elementos obtenidos por la propiedad 'data'  
     tags(){
+
+        //Variable array que guardara los elementos div
         let options = new Array();
+
+       //Variable que contiene la información 'this' de la clase
         let self = this;
 
+       //Ciclo que genera los div de todos los usuarios existentes; los div se guardan en la variable 'options'
         for(let i = 0; i < self.data.length; i++){
             options[i] = document.createElement('div');
         }
 
+        //Evento 'keyup'; se ejecuta cada vez que se escribe un caracter en el input que está guardado en la propiedad 'field'; invoca el método searchTags y recibe el parámetro de la varibe 'option' 
         self.field.addEventListener('keyup',function(){
 			self.searchTags(options);
 		});
